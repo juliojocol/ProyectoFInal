aca87902881b0a59f988c89908c06466e75b7574
Julio JOcol <juliojocol12@mesoamericana.edu.gt>
2021-12-01 19:39:46 -0600

    Implementacion de urls en django

--- pagina/views.py
+++ pagina/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404, render, get_list_or_404
+from django.utils import timezone
+
+from .models import Publicacion
+
+from .forms import PublicacionForm
 
 # Create your views here.
+def publicacion_lista(request):
+    publicaciones = Publicacion.objects.all()
+    return render(request, 'pagina/publicacion_lista.html', {'publicaciones': publicaciones})
+
+
+def publicacion_detalle(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'pagina/publicacion_detalle.html', {'publicacion':publicacion})
+
+
+def publicacion_nueva(request):
+    if request.method == "POST":
+        formulario = PublicacionForm(request.POST)
+        if formulario.is_valid():
+            publicacion = formulario.save(commit=False)
+            publicacion.autor = request.user
+            #publicacion.fecha_publicacion = timezone.now()
+            publicacion.save()
+            return redirect('publicacion_detalle', pk=publicacion.pk)
+    else:
+        formulario = PublicacionForm()
+    return render(request, 'pagina/publicacion_editar.html', {'formulario': formulario})
+
+
+def publicacion_editar(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    if request.method == "POST":
+        formulario = PublicacionForm(request.POST, instance=publicacion)
+        if formulario.is_valid():
+            publicacion = formulario.save(commit=False)
+            publicacion.autor = request.user
+            #publicacion.fecha_publicacion = timezone.now()
+            publicacion.save()
+            return redirect('publicacion_detalle', pk=publicacion.pk)
+    else:
+        formulario = PublicacionForm(instance=publicacion)    
+    return render(request, 'pagina/publicacion_editar.html', {'formulario': formulario})
+
+
+def publicacion_borrador_lista(request):
+    publicaciones = Publicacion.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
+    return render(request, 'pagina/publicacion_borrador_lista.html', {'publicaciones':publicaciones})
+
+
+def publicacion_publicar(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    publicacion.publicar()
+    return redirect('publicacion_detalle', pk=pk)
+
+
+def publicacion_eliminar(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    publicacion.delete()
+    return redirect('publicacion_lista')
